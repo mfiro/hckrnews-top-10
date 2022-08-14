502bbc8e3b4e52107dbc5bd6045ee2d25e92f4cd
mfiro <mfiro@protonmail.com>
2022-08-14 14:56:08 +0200

    [hn.py] Add more functions

--- top10/hn.py
+++ top10/hn.py
@@ -1,4 +1,6 @@
 import requests
+import datetime
+
 
 class Client:
     base_url = 'https://hacker-news.firebaseio.com/v0/'
@@ -13,4 +15,25 @@ class Client:
 
     def get_item(self, item_id):
         url = f"{self.__class__.base_url}/item/{item_id}.json"
+        item = self._request(url)
+        if item.get('time'):
+            item['time_str'] = str(datetime.datetime.fromtimestamp(item['time']))
+        return item
+
+    def get_topstories(self):
+        url = f"{self.__class__.base_url}/topstories/.json"
         return self._request(url)
+    
+    def get_beststories(self):
+        url = f"{self.__class__.base_url}/beststories/.json"
+        return self._request(url)
+    
+    def get_newstories(self):
+        url = f"{self.__class__.base_url}/newstories/.json"
+        return self._request(url)
+    
+    def get_maxitem(self):
+        url = f"{self.__class__.base_url}/maxitem/.json"
+        #datetime.fromtimestamp(user['created'])
+        return self._request(url)
+

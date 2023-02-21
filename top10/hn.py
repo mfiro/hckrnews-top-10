75c73748abc220c3eed3a4d4c35f884f2b81caee
mfiro <mfiro@protonmail.com>
2023-02-21 23:22:46 +0100

    [hn.py] -  Add some comments - Change item['time_str'] from local time to utc - Add _makeurl() -> not tested yet

--- top10/hn.py
+++ top10/hn.py
@@ -12,12 +12,21 @@ class Client:
         r = requests.get(url)
         r = r.json()
         return r
+    
+    def _makeurl(endpoint, type_='json'):
+        return f"{self.__class__.base_url}/{endpoint}.{type_}"
 
     def get_item(self, item_id):
+        """
+        item['descendants'] refers to the number of comments, if the item is story.
+        item['score'] is exactly the points shown in hckrnews.com
+        """
         url = f"{self.__class__.base_url}/item/{item_id}.json"
         item = self._request(url)
+
+        # Add a string represented time to the dict
         if item.get('time'):
-            item['time_str'] = str(datetime.datetime.fromtimestamp(item['time']))
+            item['time_str'] = str(datetime.datetime.utcfromtimestamp(item['time']))
         return item
 
     def get_topstories(self):
@@ -25,6 +34,9 @@ class Client:
         return self._request(url)
     
     def get_beststories(self):
+        """It returns the id of the best stories.
+        """
+
         url = f"{self.__class__.base_url}/beststories/.json"
         return self._request(url)
     
@@ -34,6 +46,4 @@ class Client:
     
     def get_maxitem(self):
         url = f"{self.__class__.base_url}/maxitem/.json"
-        #datetime.fromtimestamp(user['created'])
         return self._request(url)
-

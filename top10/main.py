e238d40f8d3626ee627b5163c41f51f309a5624e
mfiro <mfiro@mfiro>
2024-03-03 21:50:15 +0100

    [main.py] Adding Docstrings

--- top10/main.py
+++ top10/main.py
@@ -8,14 +8,40 @@ from top10.helpers import save_json, load_json
 
 
 def get_hn_client():
+    """
+    Initialize and return a Hacker News client instance.
+
+    Returns:
+        Client: An instance of the Hacker News client.
+    """
     return Client()
 
 
 def get_telegram_client(api_key):
+    """
+    Initialize and return a Telegram client instance.
+
+    Args:
+        api_key (str): The API key for the Telegram bot.
+
+    Returns:
+        TGClient: An instance of the Telegram client.
+    """
     return TGClient(api_key)
 
 
 def fetch_best_stories(hn_client, filename):
+    """
+    Fetches the best stories from Hacker News, retries on failure for individual stories, 
+    and saves the data to a JSON file.
+
+    Args:
+        hn_client (Client): An instance of the Hacker News client.
+        filename (str): The name of the file where the data is to be saved.
+
+    Returns:
+        list: A list of dictionaries, each containing data about a best story.
+    """
     best_stories = hn_client.get_beststories()
     stories_data = []
     for story_id in sorted(best_stories, reverse=True):
@@ -36,10 +62,29 @@ def fetch_best_stories(hn_client, filename):
 
 
 def filter_todays_stories(stories, today):
+    """
+    Filters stories to include only those published today.
+
+    Args:
+        stories (list): A list of story data dictionaries.
+        today (datetime.date): The current date.
+
+    Returns:
+        list: A list of dictionaries, each containing data about a story published today.
+    """
     return [story for story in stories if datetime.datetime.utcfromtimestamp(story['time']).date() == today]
 
 
 def create_message(stories):
+    """
+    Creates a formatted message string containing details of the stories.
+
+    Args:
+        stories (list): A list of story data dictionaries to be included in the message.
+
+    Returns:
+        str: A formatted string containing the stories' details.
+    """
     message = ""
     for no, story in enumerate(stories[:10], start=1):
         message += f"{no}. {story['title']} | [Article]({story['url']}) | [Comments ({story['descendants']})](https://news.ycombinator.com/item?id={story['id']}) | Score:{story['score']}\n"
@@ -48,6 +93,9 @@ def create_message(stories):
 
 
 def main():
+    """
+    Main function to fetch the top HN stories of the day and send them via Telegram.
+    """
     hn_client = get_hn_client()
     telegram_client = get_telegram_client(TELEGRAM_API_KEY)
     debug_mode = False
